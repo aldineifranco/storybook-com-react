22e1d77f627a596a4bdd27630418526735543038
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-09-22 13:34:21 -0300

    criação componente Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Hero = () => (
+  <div>
+    <div>
+      <h1>Ganhe sua liberdade para ir e vir</h1>
+      <ul>
+        <li>
+          is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been
+        </li>
+        <li>
+          is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been
+        </li>
+        <li>
+          is simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been
+        </li>
+        <buttom>Matricule-se Agora</buttom>
+      </ul>
+    </div>
+  </div>
+);
