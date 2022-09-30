41c4436e55ccbeb595658947e95e4111028544dd
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-09-30 08:38:39 -0300

    welcome

---
+++ src/stories/welcome.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const welcomePage = () => (
+  <div>
+    <h1>Seja Bem-vindo ao Storybook</h1>
+    <p>
+      is simply dummy text of the printing and typesetting industry.is simply
+      dummy text of the printing and typesetting industry.
+    </p>
+  </div>
+);
