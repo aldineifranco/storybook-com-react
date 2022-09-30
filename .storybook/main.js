3752bb717c93fef5dcb28553e0ae627de522e27f
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-09-30 09:18:44 -0300

    addon-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-knobs",
   ],
   framework: "@storybook/react",
   core: {
