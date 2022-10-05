8c0ec7f0bf54659770f226b1eba22b3614c588ad
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-10-05 11:41:43 -0300

    Feature

---
+++ src/components/Atoms/Feature.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Root = styled.div`
+  text-align: center;
+
+  & h5 {
+    margin: 16px 0;
+  }
+
+  & p {
+    margin: 0;
+  }
+`;
+
+const Feature = ({ icon, title, children }) => (
+  <Root>
+    <div>{icon}</div>
+    <h5>{title}</h5>
+    <div>{children}</div>
+  </Root>
+);
+
+Feature.defaultProps = {
+  icon: undefined,
+  title: "",
+  children: undefined,
+};
+
+Feature.propTypes = {
+  icon: PropTypes.node,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Feature;
