5586e637e06808fb53394dfac9e8b3dec2d5b42c
Aldinei <aldinei.franco@unimedprudente.com.br>
2022-10-07 17:00:11 -0300

    Accordion  - estrutura e comportamento

---
+++ src/components/Atoms/Accordion.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { MdExpandMore, MdExpandLess } from "react-icons/md";
+
+const Root = styled.div`
+  border: 1px solid #e0e0e0;
+  border-left: 5px solid ${(props) => props.theme.colors.primary.main};
+  background-color: #fff;
+`;
+
+const Head = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+
+  h6 {
+    margin: 0;
+    flex: 1;
+  }
+
+  & svg {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme.colors.primary.main};
+  }
+`;
+
+const Body = styled.div`
+  padding: 0 16px 16px 16px;
+`;
+
+const Accordion = ({ title, children }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Root>
+      <Head role="button" onClick={handleClick}>
+        <h6>{title}</h6>
+        {open ? <MdExpandLess /> : <MdExpandMore />}
+      </Head>
+      {open && <Body>{children}</Body>}
+    </Root>
+  );
+};
+
+Accordion.defaultProps = {
+  title: undefined,
+  children: undefined,
+};
+
+Accordion.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Accordion;
---
+++ src/components/Atoms/Accordion.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Accordion from "./Accordion";
+
+export default {
+  title: "Components/Atoms/Accordion",
+  component: Accordion,
+};
+
+export const usage = () => (
+  <Accordion title="Título da dúvida Aqui">
+    is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+    has been the industry's standard dummy text ever since the 1500s, when an u
+  </Accordion>
+);
